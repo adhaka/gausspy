527b60efa654d2e5cef117d42f1fb1beae041275
Akash <akash.uee@gmail.com>
2016-10-31 18:23:50 +0100

    adding mle functionality to compute gp params

--- GP-Regression-scr.py
+++ GP-Regression-scr.py
@@ -8,6 +8,19 @@ import matplotlib.pyplot as plt
 
 
 KERNEL = 'noisyexp'
+x_inp = np.asarray([-1.50, -1.00, -0.75, -0.40, -0.25, 0.00])
+y_inp = np.asarray([-1.70, -1.10, -0.3, 0.20, 0.50, 0.95])
+
+
+def negLogLike(gp_params):
+	l= gp_params[0]
+	sigma_fn = gp_params[1]
+	sigma_noise = gp_params[2]
+	K_mat = computeCovMat(x_inp, sigma_fn, l, sigma_noise)
+	K_inv = np.linalg.inv(K_mat)
+
+	return (-0.5 * np.dot(np.transpose(y_inp), np.dot(K_inv, y_inp)) - 0.5*np.log(np.linalg.det(K_mat)) - 0.5*n*2*np.pi)
+
 
 
 def computeKernel(x1, x2, var=1.1, l=2.5, var_noise=0.3, fn='sqexp'):
@@ -20,15 +33,15 @@ def computeKernel(x1, x2, var=1.1, l=2.5, var_noise=0.3, fn='sqexp'):
 	return val
 
 
-def computeCovMat(x_vec):
+def computeCovMat(x_vec, var=1.1, l=2.5, var_noise=0.3, fn='sqexp'):
 	n = np.prod(x_vec.shape)
 	cov = np.zeros((n,n))
 	# if not isinstance(x_vec, ())
 	for i in xrange(n):
 		for j in xrange(n):
-			cov[i,j] = computeKernel(x_vec[i], x_vec[j], fn=KERNEL)
+			cov[i,j] = computeKernel(x_vec[i], x_vec[j], var=1.1, l=2.5, var_noise=0.3, fn=KERNEL)
 
-	print cov
+	# print cov
 	return cov
 
 
@@ -90,22 +103,48 @@ y_test_confidence_95_down = y_test_star_mean - 1.96*np.sqrt(y_test_variance)
 print y_test_star_mean.shape, x_test.shape
 # print y_test_variance
 
-
 # plt.plot(x_test, y_test_star_mean, 'g-')
 # plt.plot(x_test, y_test_confidence_95_down, 'r-')
 # plt.plot(x_test, y_test_confidence_95_up, 'r-')
+# plt.plot(x_inp, y_inp, 'bo') 	
 # plt.show()
 
 
+l =2.5
+sigma_fn =1.1
+sigma_noise = 0.3
 
-# gp_params = [l, sigma_fn, sigma_noise]
+gp_params = [l, sigma_fn, sigma_noise]
 
 # calculate data likelihood for these params
 n = np.prod(y_inp.shape)
 
-nll = -0.5 * np.dot(np.transpose(y_inp), np.dot(K_inv, y_inp)) - 0.5*np.log(np.linalg.det(K_mat)) - 0.5*n*2*np.pi
+# nll = -0.5 * np.dot(np.transpose(y_inp), np.dot(K_inv, y_inp)) - 0.5*np.log(np.linalg.det(K_mat)) - 0.5*n*2*np.pi
 
+nll = negLogLike(gp_params)
+p0 = [4, 1.1, 0.7]
 print nll
+
+p_full = [p0, x_inp, y_inp]
+# cons = ({'type': 'ineq', 'fun': lambda x:  x[0] - 2 * x[1] + 2},
+# ...         {'type': 'ineq', 'fun': lambda x: -x[0] - 2 * x[1] + 6},
+# ...         {'type': 'ineq', 'fun': lambda x: -x[0] + 2 * x[1] + 2})
+
+
+# constraints = ({'type':'ineq', 'fun': lambda x: })
+bnds = ((0, None), (0, None), (0, None))
+options={'xtol': 1e-8, 'disp': True}
+
+results = minimize(negLogLike, p0, method='Newton-CG', bounds=bnds, options=options)
+print results.x
+# res = optimize.fmin(negLogLike, , args=p0)
+
+
+
+# use optimisation function to minimise the likelihood wrt the parameters.
+
+
+
 
 
 
